d653ca8da350048ecce2903236c61e0e442ffcca
Sam Plank <samuelplank@college.harvard.edu>
2022-01-17 00:35:15 -0500

    firebase

--- functions.js
+++ functions.js
@@ -1,8 +1,23 @@
+var db = firebase.firestore();
+var email;
+
 function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  email = profile.getEmail();
   var inputform = document.getElementById("input-form");
   inputform.disabled = false;
   var urlform = document.getElementById("url");
-  urlform.value = "url of file"
+  urlform.value = "url of file";
+  db.collection("users").add({
+      email: email
+  })
+  .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+  })
+  .catch((error) => {
+      console.error("Error adding document: ", error);
+  });
+
 }
 
 function signOut() {
@@ -15,4 +30,19 @@ function signOut() {
   inputform.disabled = true;
   var urlform = document.getElementById("url");
   urlform.value = "Sign in to use Dubb"
-} 
+}
+
+function onSubmit() {
+  var urlform = document.getElementById("url");
+  var urlinput = urlform.value;
+  db.collection("requests").add({
+      email: email,
+      url: urlinput
+  })
+  .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+  })
+  .catch((error) => {
+      console.error("Error adding document: ", error);
+  });
+}
