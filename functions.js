c68841f2371af9200b05efffc3c3177c49fcd0d0
Sam Plank <samuelplank@college.harvard.edu>
2022-01-16 21:52:34 -0500

    disable

--- functions.js
+++ functions.js
@@ -2,5 +2,12 @@ function onSignIn(googleUser) {
   var inputform = document.getElementById("input-form");
   inputform.disabled = false;
   var urlform = document.getElementById("url");
-  urlform.value = "url of file (can be youeube, spotify)"
+  urlform.value = "url of file"
 }
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+} 
