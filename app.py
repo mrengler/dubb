bba941db0c0215b934312e8ac635fdb4fec0201c
Sam Plank <samuelplank@college.harvard.edu>
2022-02-27 10:03:18 -0800

    boolean

--- app.py
+++ app.py
@@ -6,19 +6,23 @@ app = Flask(__name__)
 
 # get_transcript()
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('templates/index.html')
 
 @app.route('../transcribe', methods=['GET', 'POST'])
 def transcribe():
-    output = request.get_json()
-    print(output) # This is the output that was stored in the JSON within the browser
-    print(type(output))
-    result = json.loads(output) #this converts the json output to a python dictionary
-    print(result) # Printing the new dictionary
-    print(type(result))#this shows the json converted as a python dictionary
-    return result
+    if request.method == 'POST':
+
+        output = request.get_json()
+        print(output) # This is the output that was stored in the JSON within the browser
+        print(type(output))
+        result = json.loads(output) #this converts the json output to a python dictionary
+        print(result) # Printing the new dictionary
+        print(type(result))#this shows the json converted as a python dictionary
+        return result
+    else:
+        return 'else'
 
 if __name__ == '__main__':
   app.run(debug=True)
