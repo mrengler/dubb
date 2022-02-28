ee6b2ba99056e2b535346b1395b0e9133cd33e8f
Sam Plank <samuelplank@college.harvard.edu>
2022-02-27 16:34:05 -0800

    templates folder

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
 @app.route('/transcribe', methods=['GET', 'POST'])
 def transcribe():
