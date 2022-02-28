6ca45463f15a681f2204cd8fe1dfd7be59b82efa
Sam Plank <samuelplank@college.harvard.edu>
2022-02-27 16:40:18 -0800

    templates folder

--- app.py
+++ app.py
@@ -6,12 +6,8 @@ app = Flask(__name__)
 
 # get_transcript()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/transcribe', methods=['GET', 'POST'])
-def transcribe():
     if request.method == 'POST':
 
         output = request.get_json()
@@ -22,7 +18,21 @@ def transcribe():
         print(type(result))#this shows the json converted as a python dictionary
         return result
     else:
-        return 'else'
+        return render_template('index.html')
+
+# @app.route('/transcribe', methods=['GET', 'POST'])
+# def transcribe():
+#     if request.method == 'POST':
+
+#         output = request.get_json()
+#         print(output) # This is the output that was stored in the JSON within the browser
+#         print(type(output))
+#         result = json.loads(output) #this converts the json output to a python dictionary
+#         print(result) # Printing the new dictionary
+#         print(type(result))#this shows the json converted as a python dictionary
+#         return result
+#     else:
+#         return 'else'
 
 if __name__ == '__main__':
   app.run(debug=True)
