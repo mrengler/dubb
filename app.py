24535f157b6cc4ad931c4811ea970c8a21365154
Sam Plank <samuelplank@college.harvard.edu>
2022-04-11 23:48:46 -0400

    lengthen time

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ complete_end_string = "+++"
 # ]
 # transcript_id='os0h1626vt-d735-4829-8aab-250d32664e75'
 
-q = Queue(connection=conn, default_timeout=1200)
+q = Queue(connection=conn, default_timeout=3600)
 
 
 def get_template(data, refresh=False):
