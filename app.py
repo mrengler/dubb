f9b8eb1aaca9c2e4ad711aca0d632804456eacd7
Sam Plank <samuelplank@college.harvard.edu>
2022-05-01 22:28:54 -0400

    firebase

--- app.py
+++ app.py
@@ -30,7 +30,20 @@ complete_end_string = "+++"
 
 q = Queue(connection=conn, default_timeout=3600)
 
-cred = credentials.Certificate('creds/fbconfig.json')
+ENV_KEYS = {
+    "type": "service_account",
+    "project_id": os.environ["FIREBASE_PROJECT_ID"]
+    "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
+    "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n",
+    "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
+    "client_id": os.environ["FIREBASE_CLIENT_ID"],
+    "auth_uri": os.environ["FIREBASE_AUTH_URI"]
+    "token_uri": os.environ["FIREBASE_TOKEN_URI"],
+    "auth_provider_x509_cert_url": os.environ["FIREBASE_AUTH_PROVIDER_x509_cert_url"],
+    "client_x509_cert_url": os.environ["CLIENT_x509_CERT_URL"],
+}
+
+cred = credentials.Certificate(ENV_KEYS)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
@@ -121,7 +134,7 @@ def process():
                 },
                 timeout=600
             )
-            
+
             db.collection("requests").document().set({
                 'email': email,
                 'url': url,
