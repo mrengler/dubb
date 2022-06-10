895fd93b603c191cf73eacd20d7689eb848a270e
Sam Plank <samuelplank@college.harvard.edu>
2022-06-09 20:46:36 -0400

    log user

--- static/js/functions.js
+++ static/js/functions.js
@@ -43,17 +43,6 @@ function onSignIn(googleUser) {
     })      
   }
 
-  var d = new Date(Date.now()).toString();
-  db.collection("users").add({
-      email: email,
-      time: d
-  })
-  .then((docRef) => {
-      console.log("Document written with ID: ", docRef.id);
-  })
-  .catch((error) => {
-      console.error("Error adding document: ", error);
-  });
   var signout = document.getElementById("sign-out");
   var signin = document.getElementById("sign-in");
 }
