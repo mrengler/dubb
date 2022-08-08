e5b466135ce1b9536925ba8e4c20547f6ef5c76f
Sam Plank <samuelplank@college.harvard.edu>
2022-08-08 10:29:42 -0400

    redis

--- worker.py
+++ worker.py
@@ -9,8 +9,9 @@ listen = ['high', 'default', 'low']
 # redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 
 # conn = redis.from_url(redis_url)
-url = urlparse(os.environ.get("REDIS_URL"))
-conn = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
+# url = urlparse(os.environ.get("REDIS_URL"))
+# conn = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
+redis.from_url(os.environ.get("REDIS_URL"))
 
 if __name__ == '__main__':
     with Connection(conn):
