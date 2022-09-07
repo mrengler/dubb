62fcbcc101eac71b51fefd7b29c9ce9c5f0a7025
Sam Plank <samuelplank@college.harvard.edu>
2022-09-07 00:46:38 -0400

    remove second mail

--- app.py
+++ app.py
@@ -122,21 +122,6 @@ def result(id):
         return get_template(refresh=True)
     elif status == 'finished':
         print(job.result)
-        result, email = job.result
-        # result, email, prompt_chunks = job.result
-
-        response = requests.\
-            post("https://api.mailgun.net/v3/%s/messages" % app.config['MAILGUN_DOMAIN'],
-                auth=("api", app.config['MAILGUN_API_KEY']),
-                 data={
-                     "from": 'dubb@'+ str(app.config['MAILGUN_DOMAIN']),
-                     "to": str(app.config['MAIL_USERNAME']), ## to be updated to email
-                     "subject": "Dubb results",
-                     "text": result,
-                     "html": result
-                 }
-             )
-        print(response.raise_for_status()) 
 
         return get_template()
 
