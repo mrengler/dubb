46f04be1382cfc82d19b66a784b3a5b1242aaee7
Sam Plank <samuelplank@college.harvard.edu>
2022-09-15 19:46:05 -0400

    debug

--- helper_functions.py
+++ helper_functions.py
@@ -161,6 +161,9 @@ def assembly_finish_transcribe(transcript_id, speakers_input, paragraphs):
     sentences_diarized = [(sentence['words'][0]['speaker'], sentence['text'], millsecond_to_timestamp(sentence['start']), sentence['start']) for sentence in sentences]
     speakers_duplicate = [speaker for speaker, sentence, start_time, start_time_unformatted in sentences_diarized]
     unique_speakers = list(dict.fromkeys(speakers_duplicate))
+    if len(unique_speakers < len(speakers_input)):
+        speakers_input = [:len(unique_speakers)]
+
     speaker_hash = {}
     for i,speaker in enumerate(speakers_input):
         speaker_hash[unique_speakers[i]] = speaker
