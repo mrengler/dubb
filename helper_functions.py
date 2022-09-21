697b273a4fc170bed048a45c5b43f3b0fe3e1f38
Sam Plank <samuelplank@college.harvard.edu>
2022-09-21 01:19:43 -0400

    add audio images

--- helper_functions.py
+++ helper_functions.py
@@ -396,7 +396,8 @@ def convert(
 
     replicate_model = replicate.models.get("deforum/deforum_stable_diffusion")
     
-    os.mkdir(dir_name)
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
 
     summary_chunks = []
     top_quotes = []
