1d5ecd990a1c39238ba5b3c1168b09d236de77b0
Sam Plank <samuelplank@college.harvard.edu>
2022-09-24 16:19:27 -0400

    debug finish transcribe

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def result(id):
         for image_audio_filename in image_audio_filenames:
             download_from_gs('writersvoice', image_audio_filename, image_audio_filename)        
         # print('This is audio_clips_filenames')
-        print(audio_filenames)
+        print(os.listdir())
         # present_audio_clips = """<audio controls><source src='""" + """' type='audio/mpeg'></audio><br><br><video autoplay controls><source src='""".join(audio_filenames) + """' type='audio/mpeg'></audio>"""
         # combined += """<br><br><b><a id="audio_clips">Audio Clips</a></b><br><br>""" + present_audio_clips
         return get_template(combined)
