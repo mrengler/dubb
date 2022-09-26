56b9eb67a6f64b2ae15e3cafbfd849e9925977c5
Sam Plank <samuelplank@college.harvard.edu>
2022-09-26 02:45:55 -0400

    debug finish transcribe

--- helper_functions.py
+++ helper_functions.py
@@ -642,10 +642,10 @@ def run_combined(
 
     present_image_audio_clips = """<video autoplay controls><source src='""" + """' type='video/mp4'></video><br><br><video autoplay controls><source src='""".join(image_audio_filenames) + """' type='video/mp4'></video>"""
     
-    if audio != None:
-        present_audio_clips = """<audio controls><source src='""" + """' type='audio/mpeg'></audio><br><br><video autoplay controls><source src='""".join(audio_filenames) + """' type='audio/mpeg'></audio>"""
-    else:
-        present_audio_clips = ""
+    # if audio != None:
+    present_audio_clips = """<audio controls><source src='""" + """' type='audio/mpeg'></audio><br><br><video autoplay controls><source src='""".join(audio_filenames) + """' type='audio/mpeg'></audio>"""
+    # else:
+    #     present_audio_clips = ""
 
     l1 = [chunk.replace('\n', '\n\n') for chunk in summary_chunks]
     l2 = [chunk.replace('\n\n\n\n', '\n\n') for chunk in l1]
