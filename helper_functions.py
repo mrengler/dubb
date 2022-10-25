300a43e009b2a0ffab9021241effc593f36d19e4
Sam Plank <samuelplank@college.harvard.edu>
2022-10-24 23:31:49 -0400

    undo print paragraphs

--- helper_functions.py
+++ helper_functions.py
@@ -185,8 +185,6 @@ def assembly_finish_transcribe(transcript_id, speakers_input, paragraphs):
     response = requests.get(endpoint, headers=headers)
     
     try:
-        print('this is response paragraphs')
-        print(response.json()['paragraphs'])
         sentences = response.json()['sentences']
         sentences_diarized = [(sentence['words'][0]['speaker'], sentence['text'], millsecond_to_timestamp(sentence['start']), sentence['start']) for sentence in sentences]
         speakers_duplicate = [speaker for speaker, sentence, start_time, start_time_unformatted in sentences_diarized]
