51ae013d4b472bf7fb5ddded8631aed8b19e076c
Sam Plank <samuelplank@college.harvard.edu>
2022-11-18 14:11:08 -0500

    debug find speaker

--- helper_functions.py
+++ helper_functions.py
@@ -194,47 +194,47 @@ def assembly_finish_transcribe(transcript_id, speakers_input, paragraphs, user):
         speakers_duplicate = [speaker for speaker, sentence, start_time, start_time_unformatted in sentences_diarized]
         unique_speakers = list(dict.fromkeys(speakers_duplicate))
 
-        speaker_hash = {}
-        for unique_speaker in unique_speakers:
-            window_len = 20
-            num_occurences = 100
-            speaker_appearances = [i for i, (speaker, text, timestamp_formatted, timestamp_unformatted) in enumerate(sentences_diarized) if speaker == unique_speaker]
-            speaker_appearances = speaker_appearances[:num_occurences]
-            window = []
-            for appearance_i in speaker_appearances:
-                window_i = ['Speaker ' + speaker + ": " + text for (speaker, text, _, _) in sentences_diarized[max(appearance_i - window_len, 0):min(appearance_i + window_len, len(sentences_diarized))]]
-                window += window_i
-            window = list(dict.fromkeys(window))
+        # speaker_hash = {}
+        # for unique_speaker in unique_speakers:
+        #     window_len = 20
+        #     num_occurences = 100
+        #     speaker_appearances = [i for i, (speaker, text, timestamp_formatted, timestamp_unformatted) in enumerate(sentences_diarized) if speaker == unique_speaker]
+        #     speaker_appearances = speaker_appearances[:num_occurences]
+        #     window = []
+        #     for appearance_i in speaker_appearances:
+        #         window_i = ['Speaker ' + speaker + ": " + text for (speaker, text, _, _) in sentences_diarized[max(appearance_i - window_len, 0):min(appearance_i + window_len, len(sentences_diarized))]]
+        #         window += window_i
+        #     window = list(dict.fromkeys(window))
 
-            # first_appearance_i = next(i for i, (speaker, text, timestamp_formatted, timestamp_unformatted) in enumerate(sentences_diarized) if speaker == unique_speaker)
-            # window = ['Speaker ' + speaker + ": " + text for (speaker, text, _, _) in sentences_diarized[max(first_appearance_i - window_len, 0):min(first_appearance_i + window_len, len(sentences_diarized))]]
-            find_speaker_input = '\n\n'.join(window)
-            ## reduce to fit into model window
-            buffer = 250
-            find_speaker_input = find_speaker_input[:int((max_tokens_output_base_model - buffer) * chars_per_token)]
+        #     # first_appearance_i = next(i for i, (speaker, text, timestamp_formatted, timestamp_unformatted) in enumerate(sentences_diarized) if speaker == unique_speaker)
+        #     # window = ['Speaker ' + speaker + ": " + text for (speaker, text, _, _) in sentences_diarized[max(first_appearance_i - window_len, 0):min(first_appearance_i + window_len, len(sentences_diarized))]]
+        #     find_speaker_input = '\n\n'.join(window)
+        #     ## reduce to fit into model window
+        #     buffer = 250
+        #     find_speaker_input = find_speaker_input[:int((max_tokens_output_base_model - buffer) * chars_per_token)]
 
-            choose_pre = """The transcript:\n\n"""
-            choose_post = """\n\n\nWhat is Speaker """ + unique_speaker + """'s name?:\n\nSpeaker """ + unique_speaker + ' is "'
-            choose_text = choose_pre + find_speaker_input + choose_post
-            print('This is choose_text for Speaker ' + unique_speaker)
-            print(choose_text)
-            choose = openai.Completion.create(
-                        model='text-davinci-002',
-                        prompt=choose_text,
-                        max_tokens=20,
-                        temperature=0.9,
-                        presence_penalty=0.0,
-                        user=user,
-                        stop='"',
-                        n=3
-                    )
-            print('this is choose')
-            print(choose)
-            predicted_speaker = choose.choices[0].text
-            print('This is predicted speaker for Speaker ' + unique_speaker)
-            print(predicted_speaker)
-            speaker = process.extract(predicted_speaker, speakers_input, limit=1)[0][0]
-            speaker_hash[unique_speaker] = speaker
+        #     choose_pre = """The transcript:\n\n"""
+        #     choose_post = """\n\n\nWhat is Speaker """ + unique_speaker + """'s name?:\n\nSpeaker """ + unique_speaker + ' is "'
+        #     choose_text = choose_pre + find_speaker_input + choose_post
+        #     print('This is choose_text for Speaker ' + unique_speaker)
+        #     print(choose_text)
+        #     choose = openai.Completion.create(
+        #                 model='text-davinci-002',
+        #                 prompt=choose_text,
+        #                 max_tokens=20,
+        #                 temperature=0.9,
+        #                 presence_penalty=0.0,
+        #                 user=user,
+        #                 stop='"',
+        #                 n=3
+        #             )
+        #     print('this is choose')
+        #     print(choose)
+        #     predicted_speaker = choose.choices[0].text
+        #     print('This is predicted speaker for Speaker ' + unique_speaker)
+        #     print(predicted_speaker)
+        #     speaker = process.extract(predicted_speaker, speakers_input, limit=1)[0][0]
+        #     speaker_hash[unique_speaker] = speaker
 
         current_speaker_sentences_joined = ''
 
@@ -249,7 +249,7 @@ def assembly_finish_transcribe(transcript_id, speakers_input, paragraphs, user):
             start_times = []
             start_times_unformatted = []
             for speaker, sentence, start_time, start_time_unformatted in sentences_diarized:
-                speaker = speaker_hash[speaker]
+                # speaker = speaker_hash[speaker]
                 if (speaker != current_speaker) or (num_sentences_used >= max_num_sentences):
                     if current_speaker != '':
                         current_speaker_sentences_joined = current_speaker + ": " + " ".join(current_speaker_sentences)
@@ -303,11 +303,11 @@ def assembly_finish_transcribe(transcript_id, speakers_input, paragraphs, user):
             cleaned_paragraphs.append(current_speaker_sentences_joined)
 
             ## filter ads
-            prompt = 'The transcript:\n\n' + '[' + str(start_time) + '] ' + current_speaker_sentences_joined + '\n\nIs this a transcript for an ad or promotion? Respond with either "yes" or "no".'
-            print(prompt)
+            ad_prompt = 'The transcript:\n\n' + '[' + str(start_time) + '] ' + current_speaker_sentences_joined + '\n\nIs this the transcript of an ad? Respond with either "yes" or "no".'
+            print(ad_prompt)
             is_ad_response = openai.Completion.create(
                 model='text-davinci-002',
-                prompt=prompt,
+                prompt=ad_prompt,
                 max_tokens=max_tokens_output_is_ad,
                 temperature=0.0,
                 user=user,
@@ -316,11 +316,77 @@ def assembly_finish_transcribe(transcript_id, speakers_input, paragraphs, user):
             is_ad_response = is_ad_response.choices[0].text
             print(is_ad_response)
             is_ad_response = is_ad_response.lower()
-            if 'no' in is_ad_response:
+
+            ## filter promos
+            promo_prompt = 'The transcript:\n\n' + '[' + str(start_time) + '] ' + current_speaker_sentences_joined + '\n\nIs this the transcript of a promotion for another podcast? Respond with either "yes" or "no".'
+            print(promo_prompt)
+            is_promo_response = openai.Completion.create(
+                model='text-davinci-002',
+                prompt=promo_prompt,
+                max_tokens=max_tokens_output_is_ad,
+                temperature=0.0,
+                user=user,
+            )
+
+            is_promo_response = is_promo_response.choices[0].text
+            print(is_promo_response)
+            is_promo_response = is_promo_response.lower()
+
+            if ('no' in is_ad_response) and ('no' in is_promo_response):
                 cleaned_paragraphs_no_ads.append(current_speaker_sentences_joined)
+
+            ## determine the names of the speakers
+            speaker_hash = {}
+            for unique_speaker in unique_speakers:
+                window_len = 10
+                num_occurences = 100
+                speaker_appearances = [i for i, line in enumerate(cleaned_paragraphs_no_ads) if line.split(':')[0] == unique_speaker]
+                speaker_appearances = speaker_appearances[:num_occurences]
+                window = []
+                for appearance_i in speaker_appearances:
+                    window_i = [line in cleaned_paragraphs_no_ads[max(appearance_i - window_len, 0):min(appearance_i + window_len, len(cleaned_paragraphs_no_ads))]]
+                    window += window_i
+                window = list(dict.fromkeys(window))
+
+                # first_appearance_i = next(i for i, (speaker, text, timestamp_formatted, timestamp_unformatted) in enumerate(sentences_diarized) if speaker == unique_speaker)
+                # window = ['Speaker ' + speaker + ": " + text for (speaker, text, _, _) in sentences_diarized[max(first_appearance_i - window_len, 0):min(first_appearance_i + window_len, len(sentences_diarized))]]
+                find_speaker_input = '\n\n'.join(window)
+                ## reduce to fit into model window
+                buffer = 250
+                find_speaker_input = find_speaker_input[:int((max_tokens_output_base_model - buffer) * chars_per_token)]
+
+                choose_pre = """The transcript:\n\n"""
+                choose_post = """\n\n\nWhat is Speaker """ + unique_speaker + """'s name?:\n\nSpeaker """ + unique_speaker + ' is "'
+                choose_text = choose_pre + find_speaker_input + choose_post
+                print('This is choose_text for Speaker ' + unique_speaker)
+                print(choose_text)
+                choose = openai.Completion.create(
+                            model='text-davinci-002',
+                            prompt=choose_text,
+                            max_tokens=20,
+                            temperature=0.9,
+                            presence_penalty=0.0,
+                            user=user,
+                            stop='"',
+                            n=3
+                        )
+                print('this is choose')
+                print(choose)
+                predicted_speaker = choose.choices[0].text
+                print('This is predicted speaker for Speaker ' + unique_speaker)
+                print(predicted_speaker)
+                speaker = process.extract(predicted_speaker, speakers_input, limit=1)[0][0]
+                speaker_hash[unique_speaker] = speaker
+
+            cleaned_paragraphs = [speaker_hash[line.split(':')[0]] + ':' + ':'.join(line.split(':')[1:]) for line in cleaned_paragraphs]
+            cleaned_paragraphs_no_ads = [speaker_hash[line.split(':')[0]] + ':' + ':'.join(line.split(':')[1:]) for line in cleaned_paragraphs_no_ads]
+
+            print(cleaned_paragraphs)
+            print(cleaned_paragraphs_no_ads)    
 
             return cleaned_paragraphs, start_times, cleaned_paragraphs_no_ads, start_times_unformatted, sentences_diarized
 
+        ##TODO this should be cleaned up since we're never using paragraphs==False
         elif paragraphs==False:
 
             cleaned_sentences = [speaker_hash[speaker] + ": " +  sentence for speaker, sentence, start_time, start_time_unformatted in sentences_diarized]
