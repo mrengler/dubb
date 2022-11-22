fc438865247a2457bacfa7a9b79e14fad12ceeb9
Sam Plank <samuelplank@college.harvard.edu>
2022-11-22 11:21:34 -0500

    auth

--- static/js/functions.js
+++ static/js/functions.js
@@ -46,6 +46,20 @@ var email;
 //     });
 // }
 
+function isUserEqual(googleUser, firebaseUser) {
+  if (firebaseUser) {
+    var providerData = firebaseUser.providerData;
+    for (var i = 0; i < providerData.length; i++) {
+      if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()) {
+        // We don't need to reauth the Firebase connection.
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 function onSignIn(googleUser) {
 
   console.log('Google Auth Response', googleUser);
