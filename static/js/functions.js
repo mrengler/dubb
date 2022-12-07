9555733b569e36d104394dbdbb36251b3e5d636e
Sam Plank <samuelplank@college.harvard.edu>
2022-12-07 01:11:44 -0500

    nit

--- static/js/functions.js
+++ static/js/functions.js
@@ -294,9 +294,9 @@ window.onload=function(){
     var googleUser = gapi.auth2.getAuthInstance().currentUser.get();
     onSignIn(googleUser);
   } 
-  // else if (loggedin === false) {
-  //   signOut();
-  // }
+  else if (loggedin === false) {
+    signOut();
+  }
   // testingSignIn();
   // UNCOMMENT WHEN DONE WITH TESTING
 
